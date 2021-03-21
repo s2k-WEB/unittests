7f09d72e5c6e412c0be24f570da3fb583977265c
smokey2k <datas2k@gmail.com>
2021-03-21 18:59:54 +0100

    notEqual #2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const hypotenuse = (a, b) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(
 const surface = (a,b,c) => 2*(a * b + a * c + b * c );
 const volume = (a,b,c) => a * b *c;
 const sqrt_not_nulll = (a) => Math.sqrt(a);
-
+const mul_4_numbs = (a,b,c,d) => a * b * c * d;
 
 QUnit.module('amean', function() {
     QUnit.test('Should calculate arithmetic mean.', function(assert) {
@@ -39,5 +39,11 @@ QUnit.module('volume', function() {
 QUnit.module('sqrt_not_nulll', function() {
   QUnit.test('Square root of a number not zero !', function(assert) {
     assert.notEqual(sqrt_not_nulll(16), 0, '√16 != 0');
+  });
+});
+
+QUnit.module('mul_4_numbs', function() {
+  QUnit.test('Square root of a number not zero !', function(assert) {
+    assert.notEqual(mul_4_numbs(5,7,8,9), 0, '5 * 7 * 8 * 9 != 0');
   });
 });
